9cfcacd325b3891913bdd8e15f8e176413ff5504
unknown <johmdoe@example.com>
2024-04-11 18:02:00 -0500

    is

--- Demo/src/paquete2/Demo002.java
+++ Demo/src/paquete2/Demo002.java
@@ -6,11 +6,12 @@
 package paquete2;
 import paquete1.Mensaje;
 import paquete3.Informacion;
+import java.util.Scanner;
 /**
  *
  * @author reroes
  */
-public class Demo {
+public class Demo002 {
     /**
      * @param args the command line arguments
     
@@ -19,7 +20,19 @@ public class Demo {
     public static void main(String[] args) {
         String cad = Mensaje.obtenerMensaje();
         int habitantes = Informacion.obtenerHabitantes();
+        String cadenafinal = String.format("%s con %d\n", cad,
+                habitantes);
         // usar printf;
-        System.err.printf("%s con %d\n", cad, habitantes);
-    }   
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese un numero del 1 al 5 para presentar" + 
+                "la cadena original; caso contrario se presentara en " + 
+                "mayusculas");
+        int valor = sc.nextInt();
+        
+        if(valor >=1 && valor <=5){
+            System.out.printf("%s\n", cadenafinal);
+        } else {
+            System.out.printf("%s\n", cadenafinal.toUpperCase());
+        }
+    }    
 }
